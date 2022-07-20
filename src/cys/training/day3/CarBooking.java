8fd6fc24acd30393acda14ddf7586ca50a0bfd20
selvi <selvianu@gmail.com>
2022-07-20 17:43:31 +0530

    day3 - package,scope

---
+++ src/cys/training/day3/CarBooking.java
@@ -0,0 +1,9 @@
+package cys.training.day3;
+ import java.io.*;
+public class CarBooking {
+	
+	{//It needs to be a executable block
+		System.out.println();
+	}
+
+}
---
+++ src/cys/training/day3/Operators.java
@@ -0,0 +1,13 @@
+package cys.training.day3;
+
+public class Operators {
+
+	public static void main(String[] args) {
+
+		int no1 = 59866, no2 = 100, no3;
+		{
+			no1 = no1 % 10000;
+			System.out.println(no1);
+		}
+	}
+}
